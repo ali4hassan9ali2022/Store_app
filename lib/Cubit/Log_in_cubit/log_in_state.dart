e8f90d3adafd6368ed8a290fd9a8f871e98d9daf
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2025-04-08 00:35:10 +0200

    create log in state

--- lib/Cubit/Log_in_cubit/log_in_state.dart
+++ lib/Cubit/Log_in_cubit/log_in_state.dart
@@ -2,4 +2,13 @@ abstract class LogInState {}
 
   
 class InitialLogInState extends LogInState{}
-class ChangePassworsVisibailitystate extends LogInState{}
+class LoadingLogInState extends LogInState{}
+class SuccuseLogInState extends LogInState{}
+class FailureLogInState extends LogInState{
+  final String errMessage;
+
+  FailureLogInState({required this.errMessage});
+}
+class ChangePassworsVisibailitystate extends LogInState{
+
+}
