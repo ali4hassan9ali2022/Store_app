0dbb2da2c966bee8aa068e6496823ee854bd44f7
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2025-04-07 20:43:08 +0200

    add cache helper file to my app

---
+++ lib/Core/Cache/cache_helper.dart
@@ -0,0 +1,23 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class CacheHelper {
+  static SharedPreferences? sharedPreferences;
+
+  static Future<void> init() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+  }
+
+  static Future<bool> putData({
+    required String key,
+    required bool value,
+  }) async {
+    return await sharedPreferences!.setBool(key, value);
+  }
+
+  static bool? getData({required String key}) {
+    if (sharedPreferences != null) {
+      return sharedPreferences!.getBool(key);
+    }
+    return null;
+  }
+}
