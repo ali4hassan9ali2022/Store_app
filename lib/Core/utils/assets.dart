5a351e1f2a4980f3049c8f081cec7ea3e6646375
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2025-04-07 21:58:48 +0200

    create assets images file

---
+++ lib/Core/utils/assets.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesECommerce  mobile, online, shopping, application, app, store, open, deliver
+  /// Assets/Images/e-commerce _ mobile, online, shopping, application, app, store, open, deliver.png
+  static const String imagesECommerce= "Assets/Images/e-commerce _ mobile, online, shopping, application, app, store, open, deliver.png";
+
+  /// Assets for imagesFinance, eCommerce  purchase, payment, credit card, monitor, computer, man, people
+  /// Assets/Images/finance, e-commerce _ purchase, payment, credit card, monitor, computer, man, people.png
+  static const String imagesFinance= "Assets/Images/finance, e-commerce _ purchase, payment, credit card, monitor, computer, man, people.png";
+
+  /// Assets for imagesLogistics  shipping, delivery, destination, location, application, app, scooter, deliver
+  /// Assets/Images/logistics _ shipping, delivery, destination, location, application, app, scooter, deliver.png
+  static const String imagesLogistics = "Assets/Images/logistics _ shipping, delivery, destination, location, application, app, scooter, deliver.png";
+}
+
