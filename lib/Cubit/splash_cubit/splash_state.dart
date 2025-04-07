844eed221c2e3d05e43c83c4a0305f442a3ed6bd
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2025-04-07 18:46:06 +0200

    create splash state

---
+++ lib/Cubit/splash_cubit/splash_state.dart
@@ -0,0 +1,6 @@
+abstract class SplashState {}
+class InitialSplashState extends SplashState{}
+class CartAppearSplashState extends SplashState{}
+class CartPulseSplashState extends SplashState{}
+class ProductsFlySplashState extends SplashState{}
+class CompletedSplashState extends SplashState{}
