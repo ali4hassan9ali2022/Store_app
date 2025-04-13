51b842fc40670b7e4c3bba83d9da3d87e48125fb
alikasrawy <kasrawyali47@gmail.com>
2025-04-13 07:58:00 +0200

    add states to store state

--- lib/Cubit/store_cubit/store_state.dart
+++ lib/Cubit/store_cubit/store_state.dart
@@ -4,3 +4,6 @@ abstract class StoreState {
 
 class InitialStoreState extends StoreState{}
 class ChangeBottomNavStoreState extends StoreState{}
+class LoadingStoreState extends StoreState{}
+class SuccessStoreState extends StoreState{}
+class FailureStoreState extends StoreState{}
