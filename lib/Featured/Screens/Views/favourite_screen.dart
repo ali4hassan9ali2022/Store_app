e40ca36208c92ab192a348847ca5dcd3f7d5044b
alikasrawy <kasrawyali47@gmail.com>
2025-04-22 18:39:56 +0200

    add custom favourite list view

--- lib/Featured/Screens/Views/favourite_screen.dart
+++ lib/Featured/Screens/Views/favourite_screen.dart
@@ -1,7 +1,10 @@
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:store/Cubit/store_cubit/store_cubit.dart';
+import 'package:store/Cubit/store_cubit/store_state.dart';
 import 'package:store/Featured/Screens/Widgets/custom_favourite_item.dart';
+import 'package:store/Featured/Screens/Widgets/custom_favourite_list_view.dart';
 
 class FavouriteScreen extends StatelessWidget {
   const FavouriteScreen({super.key});
@@ -9,8 +12,34 @@ class FavouriteScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var cubit = BlocProvider.of<StoreCubit>(context);
-    return const CustomFavuriteItem();
+    return BlocConsumer<StoreCubit, StoreState>(
+      listener: (context, state) {
+        if (state is SuccessChangeFavoritesStoreState) {
+          if (!state.model.status!) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                backgroundColor: Colors.deepOrange,
+
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                content: Center(child: Text(state.model.message.toString())),
+              ),
+            );
+          }
+        }
+      },
+      builder: (context, state) {
+        return ConditionalBuilder(
+          condition: cubit.homeModel != null && cubit.categoriesModel != null,
+          builder: (context) {
+            return const CustomFavouriteListView();
+          },
+          fallback: (context) {
+            return const Center(child: CircularProgressIndicator());
+          },
+        );
+      },
+    );
   }
 }
-
-
---
+++ lib/Featured/Screens/Widgets/custom_favourite_list_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:store/Featured/Screens/Widgets/custom_favourite_item.dart';
+
+class CustomFavouriteListView extends StatelessWidget {
+  const CustomFavouriteListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+        physics: const BouncingScrollPhysics(),
+        itemBuilder:
+            (context, index) => const CustomFavuriteItem(
+              
+            ),
+        separatorBuilder:
+            (context, index) =>
+                const Divider(color: Colors.grey, thickness: 1, height: 50),
+        itemCount: 10,
+      );
+  }
+}
