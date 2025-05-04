478b7f4fbc5b8fae56a8e0cf0fd79826cc6742c3
alikasrawy <kasrawyali47@gmail.com>
2025-05-04 03:29:56 +0300

    add register method to register cubit

--- lib/Cubit/register_cubit/register_cubit.dart
+++ lib/Cubit/register_cubit/register_cubit.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:store/Core/Api/cache_dio.dart';
+import 'package:store/Core/Api/end_ponits.dart';
 import 'package:store/Cubit/register_cubit/resgister_state.dart';
+import 'package:store/Models/log_in_model.dart';
 
 class RegisterCubit extends Cubit<RegisterState> {
-  RegisterCubit() :super(RegisterInitialStates());
+  RegisterCubit() : super(RegisterInitialStates());
   GlobalKey<FormState> formKey = GlobalKey();
   TextEditingController nameController = TextEditingController();
   TextEditingController emailRController = TextEditingController();
@@ -12,6 +14,7 @@ class RegisterCubit extends Cubit<RegisterState> {
   TextEditingController passwordRController = TextEditingController();
   IconData suffix = Icons.visibility_outlined;
   bool isObsecure = true;
+  late LoginModel logInModel;
   void changePasswordVisibility() {
     isObsecure = !isObsecure;
 
@@ -19,5 +22,36 @@ class RegisterCubit extends Cubit<RegisterState> {
         isObsecure ? Icons.visibility_off_outlined : Icons.visibility_outlined;
     emit(ChangePassworsVisibailitystate());
   }
-  
+
+  void userRegister() async {
+    emit(LoadingRegisterState());
+    try {
+      final response = await DioHelper.postData(
+        url: rEGISTER,
+        data: {
+          "name": nameController.text,
+          "email": emailRController.text,
+          "password": passwordRController.text,
+          "phone": phoneController.text,
+        },
+      );
+      logInModel = LoginModel.fromjson(response.data);
+      if (logInModel.status!) {
+        emit(SuccessRegisterState(loginModel: logInModel));
+      } else {
+        emit(
+          FailureRegisterState(
+            errMessage: logInModel.message ?? 'Unknown error occurred',
+          ),
+        );
+      }
+    } catch (error) {
+      print(error.toString());
+      emit(
+        FailureRegisterState(
+          errMessage: 'Failed to authenticate. Please try again later.',
+        ),
+      );
+    }
+  }
 }
--- lib/Cubit/register_cubit/resgister_state.dart
+++ lib/Cubit/register_cubit/resgister_state.dart
@@ -1,3 +1,16 @@
+import 'package:store/Models/log_in_model.dart';
+
 abstract class RegisterState {}
 class RegisterInitialStates extends RegisterState{}
 class ChangePassworsVisibailitystate extends RegisterState{}
+class LoadingRegisterState extends RegisterState{}
+class SuccessRegisterState extends RegisterState{
+final  LoginModel loginModel;
+
+  SuccessRegisterState({required this.loginModel});
+}
+class FailureRegisterState extends RegisterState{
+  final String errMessage;
+
+  FailureRegisterState({required this.errMessage});
+}
